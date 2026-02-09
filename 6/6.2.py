0d3f1d5f8d2a877a30a3cc6f65c8abfec37fcb39
Nick-Mars <nickshootnick@gmail.com>
2026-02-09 09:15:24 -0600

    6.2 New

--- 6/6.2.py
+++ 6/6.2.py
@@ -13,11 +13,16 @@ seconds = seconds % seconds_in_hour
 minutes = seconds // seconds_in_minute
 seconds = seconds % seconds_in_minute
 
-if days == 1:
-    day_word = "день"
-elif 2 <= days <= 4:
-    day_word = "дні"
-else:
+# Правильне відмінювання слова "день"
+if 11 <= days % 100 <= 14:
     day_word = "днів"
+else:
+    last_digit = days % 10
+    if last_digit == 1:
+        day_word = "день"
+    elif 2 <= last_digit <= 4:
+        day_word = "дні"
+    else:
+        day_word = "днів"
 
 print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
