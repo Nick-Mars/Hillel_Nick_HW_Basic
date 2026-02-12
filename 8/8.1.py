98841ee14a06fdc70c2508142f5fd1cdc50f813c
Nick-Mars <nickshootnick@gmail.com>
2026-02-12 12:31:47 -0600

    8

---
+++ 8/8.1.py
@@ -0,0 +1,10 @@
+def add_one(some_list):
+    number = int(''.join(map(str, some_list)))
+    number += 1
+    return [int(digit) for digit in str(number)]
+
+assert add_one([1, 2, 3, 4]) == [1, 2, 3, 5], 'Test_1'
+assert add_one([9, 9, 9]) == [1, 0, 0, 0], 'Test_2'
+assert add_one([0]) == [1], 'Test_3'
+assert add_one([9]) == [1, 0], 'Test_4'
+print('ОК')
