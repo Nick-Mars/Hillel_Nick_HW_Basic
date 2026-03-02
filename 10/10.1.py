82780b2e4b8b703da46640548a754909d24b3f58
Nick-Mars <nickshootnick@gmail.com>
2026-03-02 08:50:54 -0600

    10_12

--- 10/10.1.py
+++ 10/10.1.py
@@ -13,7 +13,6 @@ from inspect import isgenerator
 
 gen = some_gen(2, 4, pow)
 
-
-assert isgenerator(gen) == True, 'Test1'
-assert list(gen) == [2, 4, 16, 256], 'Test2'
+assert isgenerator(gen) == True, 'Test_1'
+assert list(gen) == [2, 4, 16, 256], 'Test_2'
 print('OK')
